7dd2e629a5be2d718d238c87b79a63f6c1fb6ee7
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-11 14:00:55 -0400

    --first commmit

---
+++ GiftExchange/Controllers/PresentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiftExchange.Controllers
+{
+    public class PresentsController : Controller
+    {
+        // GET: Presents
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GiftExchange/Models/Gift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiftExchange.Models
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string Contents { get; set; }
+        public string GiftHint { get; set; }
+        public string ColorWrappingPaper { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Depth { get { return Height * Width; } }
+        public double Weight { get; set; }
+        public bool IsOpened { get; set; }
+
+        
+
+
+
+    }
+}
